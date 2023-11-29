f079af6c8642e091530bddcc56c793cc4aa7f57f
Non't <90344104+Shef905015@users.noreply.github.com>
2023-11-29 09:31:34 +0100

    Add files via upload

---
+++ klasaObjektZrakoplov/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaObjektZrakoplov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Zrakoplov zrakoplov = new Zrakoplov();
+
+            zrakoplov.ToString();
+
+            zrakoplov.setNaziv("Airbus");
+            zrakoplov.setSnagaMotora(2000);
+            zrakoplov.setDosegleta(6000);
+
+            Console.WriteLine(zrakoplov.ToString());
+
+            Zrakoplov zrakoplov1 = new Zrakoplov("Cessna Skycatcher", 75, 870 );
+            Zrakoplov zrakoplov2 = new Zrakoplov("Cessna Skycatcher", 75, 870);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ klasaObjektZrakoplov/Zrakoplov.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaObjektZrakoplov
+{
+    internal class Zrakoplov
+    {
+        string naziv;
+        double snagaMotora;
+        int dosegLeta;
+
+        public void setNaziv(string naziv)
+        {
+            this.naziv = naziv;
+        }
+        public void setSnagaMotora(double snagaMotora)
+        {
+            this.snagaMotora = snagaMotora;
+        }
+        public void setDosegleta(int dosegLeta)
+        {
+            this.dosegLeta = dosegLeta;
+        }
+        //------------------------------------------
+        public string getNaziv()
+        {
+            return this.naziv;
+        }
+        public double getSnagaMotora()
+        {
+            return this.snagaMotora;
+        }
+        public int getDosegLeta()
+        {
+            return this.dosegLeta;
+        }
+        public override string ToString()
+        {
+            string ispis = "Naziv zrakoplova: " + this.getNaziv() + "\n"
+            + "Snaga motora: " + this.getSnagaMotora() + "kW\n"
+            + "Doseg leta: " + this.getDosegLeta() + "kM\n";
+            return ispis;
+        }
+
+        public Zrakoplov() { }
+
+    }
+}
